0e76e065d9fc5d81e8b7bbc245301ce4d9407979
NachoXS <pantojaignacio642@gmail.com>
2022-10-28 01:42:02 -0300

    Agregar archivos

---
+++ src/constants/menu.js
@@ -0,0 +1,28 @@
+export const drawerMenu = [
+    {
+        title: "Inicio",
+        path: "/",
+    },
+    {
+        title: "Paginas",
+        children: [
+            {
+            title: "Todo",
+            path: "/todo"
+            },
+            {
+            title: "FetchList",
+            path: "/fetch-list"
+            }
+        ]
+    }
+]
+
+export const popMenu = [
+    {
+    title: "Inicio",
+    path: "/",
+    },
+    ]
+    
+    
